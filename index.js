db0a7a9c40e6bf0449e7ab72ca48561559824351
ioseliani1 <81998936+ioseliani1@users.noreply.github.com>
2023-04-25 00:45:37 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+const Names = [["VAXO"], ["NIKA"], ["GIA"]];
+
+for(let i = 0; i < Names.length; i++){
+
+    for(let x = 0; x < Names.length; x++){
+        
+        console.log("HELLO" + " " + Names[x])
+    }
+       
+}
